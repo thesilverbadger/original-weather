4bf30dc45b42d4ff421e47fff3bc9f4b9d76533b
tom wire <tom.w.wire@gmail.com>
2013-12-09 19:26:45 +0000

    add homecontroller. add layout and home views

---
+++ Weather/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            //http://api.worldweatheronline.com/free/v1/weather.ashx?q=RG248AG&format=json&num_of_days=5&key=t77bzy6gfxxzyxvq4pu6qqea
+            return View();
+        }
+	}
+}
