8fc9426dbb7837ef4a3f96376cc1a1536e79fef3
tom wire <tom.w.wire@gmail.com>
2013-12-10 20:32:21 +0000

    removed xml formatter

--- Weather/App_Start/WebApiConfig.cs
+++ Weather/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace Weather
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
