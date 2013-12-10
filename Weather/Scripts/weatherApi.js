4be3975484ef4d28976b36f91d7dd8ae421e6eab
tom wire <tom.w.wire@gmail.com>
2013-12-10 21:40:57 +0000

    refactor functionality from localWeather.js to weatherApi.js

--- Weather/Scripts/weatherApi.js
+++ Weather/Scripts/weatherApi.js
@@ -10,7 +10,7 @@
         return $.ajax(_latLongApiUrl + "?latitude=" + latitude + "&longitude=" + longitude);
     };
 
-    var getByPostcode= function (postcode) {
+    var getByPostcode = function (postcode) {
 
         return $.ajax(_postcodeApiUrl + "?postcode=" + postcode);
     };
@@ -77,6 +77,68 @@
             $('#localTemp').html(temp);
             $('#localImage').attr("src", icon);
             $('#localWeather').show();
+        },
+
+        getLocation: function () {
+            console.log("getLocation");
+            var options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            };
+
+            if (navigator.geolocation) {
+                console.log("browser supports geolocation");
+                navigator.geolocation.getCurrentPosition(_weatherApi.getWeatherForLatLong, _weatherApi.showError, options);
+            }
+            else {
+                console.log("browser does not support geolocation");
+                var weatherLookupError = $("#weatherLookupError");
+                weatherLookupError.show();
+                weatherLookupError.html("Geolocation is not supported by this browser.");
+            }
+        },
+
+        getWeatherForLatLong: function (position) {
+
+            console.log("got position");
+
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            $("#localWeather").show();
+
+            _server.getByLatLong(latitude, longitude).done(_weatherApi.showCurrentCondition);
+
+            console.log("lat: " + latitude);
+            console.log("log: " + longitude);
+        },
+
+        showError: function (error) {
+
+            var weatherLookupError = $("#weatherLookupError");
+            weatherLookupError.show();
+            console.log("error getting position");
+            console.log(error.code);
+
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    weatherLookupError.html("User denied the request for Geolocation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    weatherLookupError.html("Location information is unavailable.");
+                    break;
+                case error.TIMEOUT:
+                    weatherLookupError.html("The request to get user location timed out.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    weatherLookupError.html("An unknown error occurred.");
+                    break;
+                default:
+                    weatherLookupError.html("An unknown error occurred.");
+                    break;
+            }
+
+            $("#postcodeLookup").show();
         },
 
     }
