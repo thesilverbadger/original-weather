3b2b13ffe6b7ecc561a34021b18001098b73102f
Tom <tom@thesilverbadger.co.uk>
2013-12-10 08:29:16 +0000

    refactor view logic out of layout to home. added some debug logging to localWeather.js

--- Weather/Scripts/localWeather.js
+++ Weather/Scripts/localWeather.js
@@ -1,43 +1,55 @@
 ﻿/// <reference path="jquery-1.10.2.intellisense.js" />
+console.log("loaded");
 
-var postcodeLookupError = $("#postcodeLookupError");
+var weatherLookupError = $("#weatherLookupError");
 
 function getLocation() {
+    console.log("getLocation");
 
     if (navigator.geolocation) {
+        console.log("browser supports geolocation");
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     }
     else {
-        postcodeLookupError.show();
-        postcodeLookupError.html("Geolocation is not supported by this browser.");
+        console.log("browser does not support geolocation");
+        alert("Geolocation is not supported by this browser.");
+        weatherLookupError.show();
+        weatherLookupError.html("Geolocation is not supported by this browser.");
     }
 }
-
-getLocation();
 
 function showPosition(position) {
 
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
+    $("#localWeather").show();
+    console.log("lat: " + latitude);
+    console.log("log: " + longitude);
 }
 
 function showError(error) {
 
-    postcodeLookupError.show();
+    weatherLookupError.show();
+    alert("Error");
+    console.log(error.code);
 
     switch (error.code) {
         case error.PERMISSION_DENIED:
-            postcodeLookupError.html("User denied the request for Geolocation.");
+            weatherLookupError.html("User denied the request for Geolocation.");
             break;
         case error.POSITION_UNAVAILABLE:
-            postcodeLookupError.html("Location information is unavailable.");
-            $("#postcodeLookup").show();
+            weatherLookupError.html("Location information is unavailable.");
             break;
         case error.TIMEOUT:
-            postcodeLookupError.html("The request to get user location timed out.");
+            weatherLookupError.html("The request to get user location timed out.");
             break;
         case error.UNKNOWN_ERROR:
-            postcodeLookupError.html("An unknown error occurred.");
+            weatherLookupError.html("An unknown error occurred.");
+            break;
+        default:
+            weatherLookupError.html("An unknown error occurred.");
             break;
     }
 }
+
+getLocation();
